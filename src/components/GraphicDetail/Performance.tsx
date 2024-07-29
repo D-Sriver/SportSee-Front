74b26a007eaee464cb61b9019cdf1b167f7b425e
D.Sriver <duez.s@proton.me>
2024-07-29 15:50:46 +0200

    Correction Performance (nom et espacements)

--- src/components/GraphicDetail/Performance.tsx
+++ src/components/GraphicDetail/Performance.tsx
@@ -23,20 +23,34 @@ export default function HexaGraphic({ userId }: Performance) {
 		return <div>Chargement des données de performance...</div>;
 	}
 
-	const formattedData = performanceData.data.map((item) => ({
+	const formattedData = [...performanceData.data].reverse().map((item) => ({
 		subject: performanceData.kind[item.kind],
 		A: item.value,
 		fullMark: 150,
 	}));
 
+	//modifier le label des données pour correspondre avec la maquette
+	const formatLabel = (value: string): string => {
+		const translations: { [key: string]: string } = {
+			cardio: 'Cardio',
+			energy: 'Énergie',
+			endurance: 'Endurance',
+			strength: 'Force',
+			speed: 'Vitesse',
+			intensity: 'Intensité',
+		};
+		return translations[value.toLowerCase()] || value;
+	};
+
 	return (
 		<div className="w-full max-w-80 max-h-80 aspect-square bg-gray-800 rounded-lg flex flex-col">
 			<ResponsiveContainer width="100%" height="100%">
 				<RadarChart cx="50%" cy="50%" outerRadius="70%" data={formattedData}>
-					<PolarGrid />
+					<PolarGrid radialLines={false} />
 					<PolarAngleAxis
 						dataKey="subject"
-						tick={{ fill: '#FFFFFF', fontSize: 10 }}
+						tick={{ fill: '#FFFFFF', fontSize: 10, dy: 3 }}
+						tickFormatter={formatLabel}
 					/>
 					<Radar
 						name="Performance"
